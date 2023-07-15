7152a12f58a516a515071fa524e18772c1bd33de
jayoung977 <wjsrnr20@gmail.com>
2023-07-15 23:08:47 +0900

    study

--- Chapter1/src/ch05/IntVariableTest.java
+++ Chapter1/src/ch05/IntVariableTest.java
@@ -6,7 +6,7 @@ public class IntVariableTest {
 		System.out.println(bs);
 		
 //		int iVal = 12345678900; //int 범위 넘어섰음 (-2^31 ~2^31-1)
-		long iVal = 12345678900L; //L을 뒤에붙여야함, 이숫자의 의미: 4바이트에 못들어가는 숫자
-		//기본자료형이 int, 4바이트이므로 이상일 경우 L을 붙여 Long이라고 표시함
+		//long iVal = 12345678900L; //L을 뒤에붙여야함, 이숫자의 의미: 4바이트에 못들어가는 숫자
+		//기본자료형이 int, 4바이트이므로 이상일 경우 L을 붙여 Long이라고 표시
 	}
 }
